f8d4994e3ba37713878ab40bf9d6b1d69b6b10ae
Mariana Marca <mariana.marca.herrera.2003@gmail.com>
2024-09-24 12:40:52 -0400

    comics

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const marvel = {
   render: () => {
+    //const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=5849d723445b45eb7da3edc5131a92cd' ;
+  const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=5849d723445b45eb7da3edc5131a92cd&hash=801e014b1bb1b0f59e2ae1a0dd877870';
     
-    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5849d723445b45eb7da3edc5131a92cd&hash=801e014b1bb1b0f59e2ae1a0dd877870';
     const container = document.querySelector('#marvel-row');
     let contentHTML = '';
 
@@ -9,14 +10,19 @@ const marvel = {
       .then(res => res.json())
       .then((json) => {
         for (const hero of json.data.results) {
-          let urlHero = hero.urls[0].url;
-          contentHTML += `
-            <div class="col-md-4">
-                <a href="${urlHero}" target="_blank">
+          // Buscar el enlace de tipo 'wiki' o cualquier otro disponible
+          let urlHero = hero.urls.find(url => url.type === "wiki") || hero.urls[0];
+          
+          // Si 'urlHero' es un objeto válido, accede a la propiedad 'url'
+          if (urlHero) {
+            contentHTML += `
+              <div class="col-md-4">
+                <a href="${urlHero.url}" target="_blank">
                   <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
                 </a>
                 <h3 class="title">${hero.name}</h3>
-            </div>`;
+              </div>`;
+          }
         }
         container.innerHTML = contentHTML;
       })
